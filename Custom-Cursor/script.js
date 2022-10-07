82fd982c77e49722fc46aec5bf13f8172bed3462
Sakib Hasan <sakib100.sa@gmail.com>
2022-10-07 19:23:55 +0600

    custom cursor complete

--- Custom-Cursor/script.js
+++ Custom-Cursor/script.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+const cursor = document.querySelector(".cursor");
+
+window.addEventListener("mousemove", (e) => {
+	cursor.style.left = e.pageX + "px";
+	cursor.style.top = e.pageY + "px";
+	cursor.setAttribute("data-fromTop", cursor.offsetTop - window.scrollY);
+});
+window.addEventListener("scroll", () => {
+	const fromTop = parseInt(cursor.getAttribute("data-fromTop"));
+	cursor.style.top = scrollY + fromTop + "px";
+});
+window.addEventListener("click", () => {
+	if (cursor.classList.contains("click")) {
+		cursor.classList.remove("click");
+		void cursor.offsetWidth;
+		cursor.classList.add("click");
+	} else {
+		cursor.classList.add("click");
+	}
+});
