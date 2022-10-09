126437d705fd4c12c55a985d20c8582b9f88ba02
Sakib Hasan <sakib100.sa@gmail.com>
2022-10-10 03:20:14 +0600

    theme-switcher

--- Dark-Theme-Switcher/script.js
+++ Dark-Theme-Switcher/script.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+const switcher = document.querySelector(".theme-switcher input");
+
+switcher.addEventListener("change", (e) => {
+	if (e.target.checked) {
+		document.body.setAttribute("data-theme", "dark");
+	} else {
+		document.body.setAttribute("data-theme", "light");
+	}
+});
