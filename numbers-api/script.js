70b6355ff3ddd0a168f8c27d7caec50ff12eb22e
Sakib Hasan <sakib100.sa@gmail.com>
2022-10-28 03:18:13 +0600

    numbers api project complete

--- numbers-api/script.js
+++ numbers-api/script.js
@@ -1,1 +1,18 @@
+const form = document.querySelector("form");
+const numberFact = document.querySelector(".numberFact");
 
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const number = e.target.querySelector(`input[type="number"]`).value;
+	const loadingText = "Wait a little bit ⌛";
+	numberFact.innerText = loadingText;
+	fetch(
+		` https://cors-anywhere.herokuapp.com/http://numbersapi.com/${number}`,
+		{ method: "GET", headers: { "X-Requested-With": "XMLHttpRequest" } }
+	)
+		.then((res) => res.text())
+		.then((data) => {
+			numberFact.innerText = data;
+			e.target.querySelector(`input[type="number"]`).value = " ";
+		});
+});
