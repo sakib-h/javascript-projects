05c0fada43a4032bbbc9858d9310752c02cfeb50
Sakib Hasan <sakib100.sa@gmail.com>
2022-10-29 01:25:52 +0600

    project complete

--- Countdown-Timer/script.js
+++ Countdown-Timer/script.js
@@ -1,1 +1,22 @@
-console.log("Connected");
+const timeHeading = document.querySelector("h1");
+let timeSecond = 90;
+
+const displayTime = (second) => {
+	const min = Math.floor(second / 60);
+	const sec = Math.floor(second % 60);
+	timeHeading.innerHTML = `${min < 10 ? "0" : ""} ${min}: ${
+		sec < 10 ? "0" : ""
+	}${sec}`;
+};
+
+displayTime(timeSecond);
+
+const countdown = setInterval(() => {
+	timeSecond--;
+	displayTime(timeSecond);
+
+	if (timeSecond === 0) {
+		clearInterval(countdown);
+		timeHeading.innerHTML = "Time's up!";
+	}
+}, 1000);
